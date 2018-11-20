4a83f8206ce2479227e6393e335e97b8e4ebe0ea
Mehdi Djo <djoughi.mehdi@gmail.com>
2018-11-20 01:02:34 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,13 +2,13 @@ module.exports = {
 
 
     google : {
-        clientID : '1092994747928-373smqv66blk656fk5m2kth214l1ve3m.apps.googleusercontent.com',
-        clientSecret : 'ZZCAOaNpJ_3A0VJUVJKRO2n7'
+        clientID : '**********',
+        clientSecret : '**********'
     },
     mongodb : {
-        dbURL : 'mongodb://mehdiDb:2491996djo@ds259253.mlab.com:59253/express_passport'
+        dbURL : '*******'
     },
     session : {
-        cookieKey : 'mehdidjo'
+        cookieKey : '*****'
     }
 };
